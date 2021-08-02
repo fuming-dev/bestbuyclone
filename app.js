97ec2bc19a3b95321956ef9c3d29a4b422e11afb
fuming-dev <fhuangasuna@gmail.com>
2021-08-02 14:51:06 -0500

    added header and a little bit of designs

---
+++ app.js
@@ -0,0 +1,21 @@
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+  function myFunction2() {
+    var x = document.getElementById("myLinks2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+
+    } else {
+      x.style.display = "block";
+    }
+  }
